b53c9be6485367fd07bfa277119656954b27fc91
xpetr2 <83075416+xpetr2@users.noreply.github.com>
2021-05-13 16:52:20 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -1,28 +1,33 @@
-# contains bunch of buggy examples
-# taken from
-# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+"""
+contains bunch of buggy examples
+taken from
+https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+"""
 from subprocess import call, Popen
 import base64
 try:
     import cPickle as pickle
-except:
+except _:
     import pickle
 
 
 # Input injection
 def transcode_file(filename):
+    """Input injection"""
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
 def foo_assert(user):
+    """Assert statements"""
     assert user.is_admin, 'user does not have access'
    # secure code...
 
 
 # Pickles
 class RunBinSh():
+    """Pickles"""
     def __reduce__(self):
         return (Popen, (('/bin/sh',),))
 
